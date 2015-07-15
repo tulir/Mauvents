a3538303ad24918ec000c3b60fa5873c42f8a563
Tulir293 <tulir293@maunium.net>
2015-07-15 13:06:04 +0300

    Comments for @TheToppeGod

--- src/net/maunium/bukkit/Mauvents/LMS.java
+++ src/net/maunium/bukkit/Mauvents/LMS.java
@@ -19,86 +19,156 @@ import net.maunium.bukkit.Maussentials.Utils.MetadataUtils;
 import net.maunium.bukkit.Maussentials.Utils.SerializableLocation;
 import net.maunium.bukkit.Maussentials.Utils.DelayedActions.DelayedTeleport;
 
+/**
+ * LMS (Last Man Standing) handler
+ * 
+ * @author Tulir293
+ * @since 1.0
+ */
 public class LMS implements Listener, IngameCommandExecutor {
 	public static final String IN_LMS = "MauventsLMSInEvent";
+	// Mauvents instance
 	private Mauvents plugin;
+	// Arena and lobby spawn points
 	private Location arena = null, lobby = null;
+	// Minimum number of players to start
 	private int minPlayers = 5;
+	// List of players in game
 	private Set<UUID> players = new HashSet<UUID>();
+	// If the game is started or not
 	private boolean started = false;
 	
 	public LMS(Mauvents plugin) {
 		this.plugin = plugin;
+		// Register listeners
 		this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		// Set command executor
 		this.plugin.getCommand("maulastmanstanding").setExecutor(this);
+		// Load configuration
 		if (plugin.getConfig().contains("lms.arena")) arena = SerializableLocation.fromString(plugin.getConfig().getString("lms.arena")).toLocation();
 		if (plugin.getConfig().contains("lms.lobby")) lobby = SerializableLocation.fromString(plugin.getConfig().getString("lms.lobby")).toLocation();
 		minPlayers = plugin.getConfig().getInt("lms.min-players", 3);
 	}
 	
+	/**
+	 * @return True if LMS is in-game.
+	 */
 	public boolean hasStarted() {
 		return started;
 	}
 	
+	/**
+	 * @return True if there are enough players to start a game.
+	 */
 	public boolean enoughPlayers() {
 		return players.size() >= minPlayers;
 	}
 	
+	/**
+	 * @return True if all the necessary settings are correctly set up.
+	 */
 	public boolean isSetUp() {
 		return arena != null && lobby != null && minPlayers > 1;
 	}
 	
+	/**
+	 * Set the arena spawn point to the given location.
+	 */
 	public void setArena(Location l) {
 		arena = l;
 		plugin.getConfig().set("lms.arena", new SerializableLocation(l).toString());
 	}
 	
+	/**
+	 * Set the lobby spawn point to the given location.
+	 * 
+	 * @param l
+	 */
 	public void setLobby(Location l) {
 		lobby = l;
 		plugin.getConfig().set("lms.lobby", new SerializableLocation(l).toString());
 	}
 	
+	/**
+	 * Start the match
+	 */
 	public void start() {
+		// Loop through UUIDs
 		for (UUID u : players) {
+			// Get the player instances
 			Player p = plugin.getServer().getPlayer(u);
+			// If found, teleport to arena
 			if (p != null && p.isOnline()) p.teleport(arena);
+			// If not found, remove from match
 			else players.remove(u);
 		}
+		// Set the started flag
 		started = true;
 	}
 	
+	/**
+	 * End the match
+	 */
 	public void end() {
-		if (players.size() == 1) {
+		// Make sure that there is less than two players left
+		if (players.size() < 2) {
+			// Get the UUID remaining player
 			UUID u = players.iterator().next();
+			// Make sure that the UUID is not null.
 			if (u != null) {
+				// Get the player instance.
 				Player pp = plugin.getServer().getPlayer(u);
-				if (pp != null) {
+				// Make sure the player is online
+				if (pp != null && pp.isOnline()) {
+					// Broadcast win message
 					plugin.getServer().broadcastMessage(plugin.translateStd("lms.win", pp.getName()));
+					// Teleport to world spawn
 					pp.teleport(plugin.getServer().getWorlds().get(0).getSpawnLocation());
+					// Remove metadata
 					MetadataUtils.removeMetadata(pp, IN_LMS, plugin);
 				} else plugin.getServer().broadcastMessage(plugin.translateStd("lms.win.null"));
 			} else plugin.getServer().broadcastMessage(plugin.translateStd("lms.win.null"));
 		} else plugin.getServer().broadcastMessage(plugin.translateStd("lms.win.null"));
+		// Clear player list
 		players.clear();
+		// Set started flag to false
 		started = false;
 	}
 	
+	/**
+	 * Join the given player to the match.
+	 */
 	public boolean join(Player p) {
+		// Make sure the player is not in-game already and that the match hasn't started.
 		if (started || players.contains(p.getUniqueId())) return false;
 		else {
+			// Add the players UUID to the player list.
 			players.add(p.getUniqueId());
+			// Add the LMS metadata
 			MetadataUtils.setFixedMetadata(p, IN_LMS, true, plugin);
+			// Send a "please stand by for tp" message
 			p.sendMessage(plugin.translateStd("lms.lobby.tping"));
+			// Create a Maussentials delayed teleport to teleport the player to the lobby.
 			new DelayedTeleport(p, 100, lobby, plugin.translateStd("lms.lobby.tped"), plugin.translateErr("lms.lobby.tpfail"), 15, 0).start();
 			return true;
 		}
 	}
 	
+	/**
+	 * Make the given player leave the match
+	 * 
+	 * @param death True if this was called from a death event.
+	 */
 	public boolean leave(Player p, boolean death) {
+		// Make sure that the player is in the game
 		if (!players.contains(p.getUniqueId()) && !p.hasMetadata(IN_LMS)) return false;
+		// Remove the LMS metadata
 		MetadataUtils.removeMetadata(p, IN_LMS, plugin);
+		// Remove the UUID from the player list
 		players.remove(p.getUniqueId());
+		// If the leaving was not caused by a death, teleport the player to the spawn.
 		if (!death) p.teleport(plugin.getServer().getWorlds().get(0).getSpawnLocation());
+		// If this was within the last two players, end the game.
 		if (started && players.size() < 2) end();
 		return true;
 	}
--- src/net/maunium/bukkit/Mauvents/Mauvents.java
+++ src/net/maunium/bukkit/Mauvents/Mauvents.java
@@ -8,10 +8,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import net.maunium.bukkit.Maussentials.Utils.I18n;
 import net.maunium.bukkit.Mauvents.Brackets.Brackets;
 
+/**
+ * Maunium Events plugin main class.
+ * 
+ * @author Tulir293
+ * @since 1.0
+ */
 public class Mauvents extends JavaPlugin {
-	private String stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Mauvents" + ChatColor.DARK_GREEN + "] " + ChatColor.GRAY, errtag = ChatColor.DARK_RED
-			+ "[" + ChatColor.RED + "Mauvents" + ChatColor.DARK_RED + "] " + ChatColor.RED;
+	private String stag = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Mauvents" + ChatColor.DARK_GREEN + "] " + ChatColor.GRAY,
+			errtag = ChatColor.DARK_RED + "[" + ChatColor.RED + "Mauvents" + ChatColor.DARK_RED + "] " + ChatColor.RED;
+	// Internationalization instance
 	private I18n i18n;
+	// Game handler instances
 	private Brackets b;
 	private LMS lms;
 	private LTS lts;
@@ -19,19 +27,26 @@ public class Mauvents extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		long st = System.currentTimeMillis();
+		// Save the default config.
 		saveDefaultConfig();
+		// Save and override the default language(s)
 		saveResource("en_US.lang", true);
 		
+		// Load I18n.
 		try {
 			i18n = I18n.createInstance(getDataFolder(), getConfig().getString("language", "en_US"));
 		} catch (IOException e) {
+			// Shit happens.
 			e.printStackTrace();
 		}
 		
+		// Use translated message prefixes
 		stag = i18n.translate("stag");
 		errtag = i18n.translate("errtag");
 		
+		// Set executor for admin command
 		getCommand("mauvents").setExecutor(new CommandMauventsAdmin(this));
+		// Create the game instances
 		b = new Brackets(this);
 		lms = new LMS(this);
 		lts = new LTS(this);
@@ -44,32 +59,57 @@ public class Mauvents extends JavaPlugin {
 	public void onDisable() {
 		long st = System.currentTimeMillis();
 		
+		// Save configuration
 		saveConfig();
 		
 		int et = (int) (System.currentTimeMillis() - st);
 		getLogger().info("Mauvents v" + getDescription().getVersion() + " by Tulir293 disabled in " + et + "ms.");
 	}
 	
+	/**
+	 * Get the Brackets handler for this Mauvents instance.
+	 */
 	public Brackets getBrackets() {
 		return b;
 	}
 	
+	/**
+	 * Get the LMS (Last Man Standing) handler for this Mauvents instance.
+	 */
 	public LMS getLMS() {
 		return lms;
 	}
 	
+	/**
+	 * Get the LTS (Last Team Standing) handler for this Mauvents instance.
+	 */
 	public LTS getLTS() {
 		return lts;
 	}
 	
+	/**
+	 * Translate the given I18n node with the given arguments and prepend the standard output tag to it.
+	 * 
+	 * @return The translated message or the node, if translation not found.
+	 */
 	public String translateStd(String node, Object... arguments) {
 		return stag + i18n.translate(node, arguments);
 	}
 	
+	/**
+	 * Translate the given I18n node with the given arguments and prepend the error output tag to it.
+	 * 
+	 * @return The translated message or the node, if translation not found.
+	 */
 	public String translateErr(String node, Object... arguments) {
 		return errtag + i18n.translate(node, arguments);
 	}
 	
+	/**
+	 * Translate the given I18n node with the given arguments.
+	 * 
+	 * @return The translated message or the node, if translation not found.
+	 */
 	public String translatePlain(String node, Object... arguments) {
 		return i18n.translate(node, arguments);
 	}
